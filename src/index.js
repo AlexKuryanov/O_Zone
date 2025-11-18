4134efb33e2d5357edf94446155da0b35afee7fe
AlexKuryanov <lexxus07@yandex.ru>
2025-11-18 12:43:16 +0100

    dev: add category filter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import cart from './modules/cart.js';
 import load from './modules/load.js';
 import search from './modules/search.js';
+import catalog from './modules/catalog.js';
 
 cart();
 load();
 search();
+catalog();
